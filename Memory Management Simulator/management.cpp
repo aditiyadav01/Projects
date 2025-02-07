68997569fd49717b72f02e5e52de1de2fc0165b1
aditiyadav01 <yadavaditi0501@gmail.com>
2025-02-07 14:10:00 +0530

    commit this file

--- Memory Management Simulator/management.cpp
+++ Memory Management Simulator/management.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 // Structure to represent a memory block
@@ -28,14 +27,14 @@ void firstFit(vector<MemoryBlock>& blocks, Process& process) {
         if (!block.allocated && block.size >= process.size) {
             block.allocated = true;
             process.allocatedBlock = block.id;
-            cout << "Process " << process.id << " allocated to block of size " << block.size << endl;
+            cout << "Process " << process.id << " allocated to block " << block.id << " (Size: " << block.size << ")\n";
             return;
         }
     }
     cout << "Process " << process.id << " cannot be allocated (First Fit)\n";
 }
 
-// Function to allocate memory using Best Fit
+// Function to allocate memory using Best Fit with splitting
 void bestFit(vector<MemoryBlock>& blocks, Process& process) {
     int bestIdx = -1;
     for (int i = 0; i < blocks.size(); i++) {
@@ -46,15 +45,19 @@ void bestFit(vector<MemoryBlock>& blocks, Process& process) {
         }
     }
     if (bestIdx != -1) {
+        if (blocks[bestIdx].size > process.size) {
+            blocks.push_back(MemoryBlock(blocks.size() + 1, blocks[bestIdx].size - process.size));
+        }
+        blocks[bestIdx].size = process.size;
         blocks[bestIdx].allocated = true;
         process.allocatedBlock = blocks[bestIdx].id;
-        cout << "Process " << process.id << " allocated to best fit block of size " << blocks[bestIdx].size << endl;
+        cout << "Process " << process.id << " allocated to best fit block " << blocks[bestIdx].id << " (Size: " << process.size << ")\n";
     } else {
         cout << "Process " << process.id << " cannot be allocated (Best Fit)\n";
     }
 }
 
-// Function to allocate memory using Worst Fit
+// Function to allocate memory using Worst Fit with retry mechanism
 void worstFit(vector<MemoryBlock>& blocks, Process& process) {
     int worstIdx = -1;
     for (int i = 0; i < blocks.size(); i++) {
@@ -67,58 +70,66 @@ void worstFit(vector<MemoryBlock>& blocks, Process& process) {
     if (worstIdx != -1) {
         blocks[worstIdx].allocated = true;
         process.allocatedBlock = blocks[worstIdx].id;
-        cout << "Process " << process.id << " allocated to worst fit block of size " << blocks[worstIdx].size << endl;
+        cout << "Process " << process.id << " allocated to worst fit block " << blocks[worstIdx].id << " (Size: " << blocks[worstIdx].size << ")\n";
     } else {
-        cout << "Process " << process.id << " cannot be allocated (Worst Fit)\n";
+        cout << "Process " << process.id << " cannot be allocated (Worst Fit), retrying...\n";
+        for (auto& block : blocks) {
+            if (!block.allocated && block.size >= process.size) {
+                block.allocated = true;
+                process.allocatedBlock = block.id;
+                cout << "Process " << process.id << " reallocated to block " << block.id << " (Size: " << block.size << ")\n";
+                return;
+            }
+        }
+        cout << "Process " << process.id << " still cannot be allocated.\n";
+    }
+}
+
+// Function to deallocate memory
+void deallocateMemory(vector<MemoryBlock>& blocks, Process& process) {
+    for (auto& block : blocks) {
+        if (block.id == process.allocatedBlock) {
+            block.allocated = false;
+            cout << "Process " << process.id << " deallocated from block " << block.id << "\n";
+            process.allocatedBlock = -1;
+            return;
+        }
     }
 }
 
 // Function to display memory block status
 void displayMemoryStatus(vector<MemoryBlock>& blocks) {
-    cout << "\nMemory Blocks Status:\n";
+    cout << "\nMemory Blocks Status:" << endl;
     for (auto& block : blocks) {
-        cout << "Block " << block.id << ": Size = " << block.size
-             << (block.allocated ? ", Allocated" : ", Not Allocated") << endl;
+        cout << "Block " << block.id << " | Size: " << block.size << " | "
+             << (block.allocated ? "Allocated" : "Free") << endl;
     }
     cout << endl;
 }
 
 int main() {
-    // Define memory blocks
-    vector<MemoryBlock> blocks = {
-        {1, 100}, {2, 200}, {3, 300}, {4, 400}, {5, 500}
-    };
-
-    // Define processes
-    vector<Process> processes = {
-        {1, 200}, {2, 100}, {3, 500}, {4, 400}, {5, 300}
-    };
+    vector<MemoryBlock> blocks = {{1, 100}, {2, 200}, {3, 300}, {4, 400}, {5, 500}};
+    vector<Process> processes = {{1, 200}, {2, 100}, {3, 500}, {4, 400}, {5, 300}};
 
     // Allocate memory using First Fit
-    cout << "\nAllocating using First Fit:\n";
-    for (auto& process : processes) {
-        firstFit(blocks, process);
-    }
+    cout << "\nAllocating using First Fit:" << endl;
+    for (auto& process : processes) firstFit(blocks, process);
     displayMemoryStatus(blocks);
 
-    // Reset memory blocks
-    for (auto& block : blocks) block.allocated = false;
+    // Reset memory blocks and deallocate processes
+    for (auto& process : processes) deallocateMemory(blocks, process);
 
     // Allocate memory using Best Fit
-    cout << "\nAllocating using Best Fit:\n";
-    for (auto& process : processes) {
-        bestFit(blocks, process);
-    }
+    cout << "\nAllocating using Best Fit:" << endl;
+    for (auto& process : processes) bestFit(blocks, process);
     displayMemoryStatus(blocks);
 
-    // Reset memory blocks
-    for (auto& block : blocks) block.allocated = false;
+    // Reset memory blocks and deallocate processes
+    for (auto& process : processes) deallocateMemory(blocks, process);
 
-    // Allocate memory using Worst Fit
-    cout << "\nAllocating using Worst Fit:\n";
-    for (auto& process : processes) {
-        worstFit(blocks, process);
-    }
+    // Allocate memory using Worst Fit with retry mechanism
+    cout << "\nAllocating using Worst Fit:" << endl;
+    for (auto& process : processes) worstFit(blocks, process);
     displayMemoryStatus(blocks);
 
     return 0;
